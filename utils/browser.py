0279ea730da945df756200cc9cff8262fa10668b
gabigab117 <gabrieltrouve5@gmail.com>
2023-12-06 22:18:42 +0100

    add display category

--- utils/browser.py
+++ utils/browser.py
@@ -1,22 +1,34 @@
 from tinydb import where
 from argon2 import PasswordHasher
 from database import db
+from rich.table import Table
+from rich.console import Console
 
 
 class AuthError(Exception):
     pass
 
 class Browser:
-    def __init__(self, username, password):
+    def __init__(self, username):
         self.hasher = PasswordHasher()
         self.users = db.table("user")
         self.username = username
-        self.auth = self.authenticate(password)
+        self.auth = False
     
     def authenticate(self, password):
         user = self.users.search(where("username") == self.username)
         if user:
-            return self.hasher.verify(user[0]["password"], password)
+            self.auth = self.hasher.verify(user[0]["password"], password)
+            return self.auth
         else:
             raise AuthError("No account for this username")
-
+    
+    def display_category(self):
+        forum = db.table("forum")
+        table = Table(title="Catégories")
+        table.add_column("Catégories")
+        for category in forum:
+            table.add_row(category["category"])
+        
+        console = Console()
+        console.print(table)
