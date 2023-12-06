b21cd704a2e3be011405a828a6c4e2862765910b
gabigab117 <gabrieltrouve5@gmail.com>
2023-12-06 22:18:10 +0100

    update message insert

--- models/topics.py
+++ models/topics.py
@@ -12,5 +12,7 @@ class Topics:
     def save(self):
         forum = db.table("forum")
         category = forum.search(where("category") == self.category)
-        category[0]["topics"].append({"title": self.title, "user": self.user, "message": [self.message]})
+        category[0]["topics"].append({"title": self.title, "user": self.user, "message": [[self.message, self.user]]})
         forum.update({"topics": category[0]["topics"]}, where("category") == self.category)
+
+# user = db.table("user").search(where("username") == "gab")
