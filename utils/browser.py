bac7c52515f5aa7025836ffb06cf1d4650e06c07
gabigab117 <gabrieltrouve5@gmail.com>
2023-12-08 02:25:15 +0100

    add a table forum attribute and signup method

--- utils/browser.py
+++ utils/browser.py
@@ -4,14 +4,25 @@ from database import db
 from rich.table import Table
 from rich.console import Console
 from security import AuthError, is_authenticate
+from models import User
 
 class Browser:
     def __init__(self, username):
         self.hasher = PasswordHasher()
         self.users = db.table("user")
+        self.forum = db.table("forum")
         self.username = username
         self.auth = False
         self.console = Console()
+    
+    def signup(self):
+        password = input("Veuillez entrer votre mot de passe ==>")
+        password2 = input("Veuillez confirmer votre mot de passe ==>")
+        if password2 != password:
+            raise AuthError("Les deux mots de passes ne sont pas identiques")
+        else:
+            User(self.username, password)
+            return f"Bienvenu(e) {self.username}"
     
     def authenticate(self, password):
         user = self.users.search(where("username") == self.username)
@@ -23,31 +34,28 @@ class Browser:
     
     def display_categories(self):
         is_authenticate(self.auth)
-        forum = db.table("forum")
         table = Table(title="Catégories")
         table.add_column("Catégories")
-        [table.add_row(category["category"]) for category in forum]
+        [table.add_row(category["category"]) for category in self.forum]
         
         self.console.print(table)
     
     def display_topics(self, category_name):
         is_authenticate(self.auth)
-        forum = db.table("forum")
         table = Table(title=f"Sujets de la catégorie {category_name}")
         table.add_column("Sujets")
         table.add_column("Auteurs")
-        category = forum.search(where("category") == category_name)
+        category = self.forum.search(where("category") == category_name)
         [table.add_row(el["title"], el["user"]) for el in category[0]["topics"]]
 
         self.console.print(table)
     
     def display_messages(self, category_name, topic_title):
         is_authenticate(self.auth)
-        forum = db.table("forum")
         table = Table(title=f"Messages du sujet {topic_title}")
         table.add_column("Messages")
         table.add_column("Auteurs")
-        category = forum.search(where("category") == category_name)
+        category = self.forum.search(where("category") == category_name)
         for el in category[0]["topics"]:
             [table.add_row(message_list[0], message_list[1]) for message_list in el["message"] if el["title"] == topic_title]
         
@@ -56,7 +64,6 @@ class Browser:
     
     def add_message(self, category_name, topic_title, message):
         is_authenticate(self.auth)
-        forum = db.table("forum")
-        category = forum.search(where("category") == category_name)
+        category = self.forum.search(where("category") == category_name)
         [el["message"].append([message, self.username]) for el in category[0]["topics"] if el["title"] == topic_title]
-        forum.update({"topics": category[0]["topics"]}, where("category") == category_name)
+        self.forum.update({"topics": category[0]["topics"]}, where("category") == category_name)
