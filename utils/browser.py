e1641eab7fdd8d5ddaeaa60ac49c51b29517805b
gabigab117 <gabrieltrouve5@gmail.com>
2023-12-09 17:06:29 +0100

    delete auth for displaying

--- utils/browser.py
+++ utils/browser.py
@@ -5,6 +5,7 @@ from rich.table import Table
 from rich.console import Console
 from security import AuthError, is_authenticate
 from models import User
+import getpass
 
 class Browser:
     def __init__(self, username):
@@ -16,15 +17,16 @@ class Browser:
         self.console = Console()
     
     def signup(self):
-        password = input("Veuillez entrer votre mot de passe ==>")
-        password2 = input("Veuillez confirmer votre mot de passe ==>")
+        password = getpass.getpass("Veuillez entrer votre mot de passe ==>")
+        password2 = getpass.getpass("Veuillez confirmer votre mot de passe ==>")
         if password2 != password:
             raise AuthError("Les deux mots de passes ne sont pas identiques")
         else:
             User(self.username, password)
             return f"Bienvenu(e) {self.username}"
     
-    def authenticate(self, password):
+    def authenticate(self):
+        password = getpass.getpass("Veuillez entrer votre mot de passe ==>")
         user = self.users.search(where("username") == self.username)
         if user:
             self.auth = self.hasher.verify(user[0]["password"], password)
@@ -33,7 +35,6 @@ class Browser:
             raise AuthError("No account for this username")
     
     def display_categories(self):
-        is_authenticate(self.auth)
         table = Table(title="Catégories")
         table.add_column("Catégories")
         [table.add_row(category["category"]) for category in self.forum]
@@ -41,7 +42,6 @@ class Browser:
         self.console.print(table)
     
     def display_topics(self, category_name):
-        is_authenticate(self.auth)
         table = Table(title=f"Sujets de la catégorie {category_name}")
         table.add_column("Sujets")
         table.add_column("Auteurs")
@@ -51,7 +51,6 @@ class Browser:
         self.console.print(table)
     
     def display_messages(self, category_name, topic_title):
-        is_authenticate(self.auth)
         table = Table(title=f"Messages du sujet {topic_title}")
         table.add_column("Messages")
         table.add_column("Auteurs")
