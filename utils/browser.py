c68d209d1ec8c95e914ed9152e5b345a2d14c752
gabigab117 <gabrieltrouve5@gmail.com>
2023-12-10 01:09:11 +0100

    add style

--- utils/browser.py
+++ utils/browser.py
@@ -36,15 +36,15 @@ class Browser:
     
     def display_categories(self):
         table = Table(title="Catégories")
-        table.add_column("Catégories")
+        table.add_column("Catégories", style="red")
         [table.add_row(category["category"]) for category in self.forum]
         
         self.console.print(table)
     
     def display_topics(self, category_name):
         table = Table(title=f"Sujets de la catégorie {category_name}")
-        table.add_column("Sujets")
-        table.add_column("Auteurs")
+        table.add_column("Sujets", style="blue")
+        table.add_column("Auteurs", style="red")
         category = self.forum.search(where("category") == category_name)
         [table.add_row(el["title"], el["user"]) for el in category[0]["topics"]]
 
@@ -52,8 +52,8 @@ class Browser:
     
     def display_messages(self, category_name, topic_title):
         table = Table(title=f"Messages du sujet {topic_title}")
-        table.add_column("Messages")
-        table.add_column("Auteurs")
+        table.add_column("Messages", style="green")
+        table.add_column("Auteurs", style="red")
         category = self.forum.search(where("category") == category_name)
         for el in category[0]["topics"]:
             [table.add_row(message_list[0], message_list[1]) for message_list in el["message"] if el["title"] == topic_title]
